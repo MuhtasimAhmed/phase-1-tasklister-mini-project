824d3325db308ffde6f976e67622aa71c6d2a2f8
Muhtasim <mts.ahmed.1010@gmail.com>
2024-02-20 02:56:10 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", (e) => {
+    e.preventDefault() 
+    const newTask = e.target.new_task_description.value
+    addToList(newTask)
+ })
 });
+function addToList(taskDescription){
+  let li = document.createElement("li")
+  li.textContent = taskDescription
+  let btn = document.createElement("button")
+  btn.textContent = "X"
+  document.querySelector("#tasks").appendChild(li)
+  document.querySelector("#tasks").appendChild(btn)
+  document.querySelector("#new_task_description").value = ""
+  }
+  
